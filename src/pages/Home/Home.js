481c0dd1110f8fb255297d533c4ca283f0c64919
Douglas <18dsc1@queensu.ca>
2023-02-17 00:46:52 -0500

    added scope 1&2 and results buttons

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 function Home(){
     return(
-        <div>
-            <h1> This is the Home Page!</h1>
+        <div style={{ display: "flex", flexDirection: 'column', position: 'relative', height: 600, alignItems:'center', justifyContent:'center'}}>
+            <button 
+            onClick={() => alert('Scope 1 & 2 under development')}>
+                Scope 1 & 2
+            </button>
+            &nbsp;
+            <button 
+            onClick={() => alert('Scope 1 & 2 Results is under development')}>
+                View Scope 1 & 2
+                Results
+            </button>
         </div>
     );
 }
