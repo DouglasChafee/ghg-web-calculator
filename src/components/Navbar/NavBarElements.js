bfd7e021891ae3fb6481d36c03b4f0fd0dfb7802
70972627 <18kwd1@queensu.ca>
2023-03-11 01:36:49 -0500

    Finished initial Profile Page

--- src/components/Navbar/NavBarElements.js
+++ src/components/Navbar/NavBarElements.js
@@ -133,3 +133,24 @@ export const NavBtnLink = styled(LinkRouter)`
     color: #010606;
   }
 `;
+
+export const ButtonLinks = styled(LinkRouter)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 10px 22px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  font-weight: bold;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #000000;
+    color: #01bf71;
+  }
+`;
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Amplify, API, Auth } from 'aws-amplify';
-import { Flex, withAuthenticator, Divider } from '@aws-amplify/ui-react';
-import { NavBtn, NavBtnLink } from "../../components/Navbar/NavBarElements";
+import { withAuthenticator, Flex, Card, Divider, Text} from '@aws-amplify/ui-react';
+import { NavBtn, ButtonLinks} from "../../components/Navbar/NavBarElements";
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from '../../aws-exports';
 Amplify.configure(awsExports);
@@ -11,9 +11,9 @@ function Profile({ signOut, user, setLogInState, setLogOutState}) {
 
     // Retrieve user profile info once every page render
     useEffect(() => {
-      console.log('attributes:', user.attributes); 
-      //callAPI()
-    }, [])
+      //console.log('attributes:', user.attributes); THIS MAKES THE COGINTO API CALL AUTOMATICALLY
+      callAPI()
+    }, [data])
   
     setLogInState("none"); // on page render disable sign-in button
     setLogOutState("flex"); // on page render enable sign-out button
@@ -37,31 +37,104 @@ function Profile({ signOut, user, setLogInState, setLogOutState}) {
 
     // Display user profile with data back on page
     return(
-        <>
-          <Flex 
-            direction="column"
-            alignItems="center"
-            justifyContent="space-around"
-            paddingBottom={"1rem"}
-            >
-            <h1>Profile {data}</h1>
-            <NavBtn>
-              <NavBtnLink to="/">Update Account Infromation</NavBtnLink>
-            </NavBtn>
-            <Divider size="large" orientation="horizontal"/>
-          </Flex>
-          <Flex 
-            direction="column"
-            alignItems="center"
-            justifyContent="space-around"
-            paddingBottom={"20rem"}
-            >
-            <h1>Group</h1>
-            <NavBtn>
-              <NavBtnLink to="/">Create New Group</NavBtnLink>
-            </NavBtn>
-            <Divider size="large" orientation="horizontal"/>
-          </Flex>
+      <>
+        <Flex direction="column" alignItems="center" wrap="wrap" marginTop="1rem" marginBottom="5rem">
+          
+          <Card variation='elevated' borderRadius="1rem" border="1px solid" paddingBottom="1px" paddingTop="1px" paddingLeft="10%" paddingRight="10%" >
+            <Flex 
+              direction="row"
+              alignItems="center"
+              >
+              <h1>Profile - </h1>
+              <NavBtn>
+                <ButtonLinks to="/">Update Information</ButtonLinks>
+              </NavBtn>
+            </Flex>
+            <Divider size="medium" display="flex" orientation="horizontal"/>
+            <Flex direction="column"
+              alignItems="flex-start"
+              >
+              <Flex 
+                marginTop="1rem" 
+                border="1px solid"
+                borderRadius="10px" 
+                width="100%"
+                > 
+                <Text padding="5px"  alignItems="center" borderRadius="10px 0px 0px 10px" borderRight="1px solid" backgroundColor="#01bf71" fontWeight={700}>First Name: </Text>
+                <Text margin="5px">Karl</Text> 
+              </Flex>
+              <Flex 
+                marginTop="1rem" 
+                border="1px solid"
+                borderRadius="10px" 
+                width="100%"
+                > 
+                <Text padding="5px"  alignItems="center" borderRadius="10px 0px 0px 10px" borderRight="1px solid" backgroundColor="#01bf71" fontWeight={700}>Last Name: </Text>
+                <Text margin="5px">Dorogy</Text> 
+              </Flex>
+              <Flex 
+                marginTop="1rem" 
+                border="1px solid"
+                borderRadius="10px" 
+                width="100%"
+                > 
+                <Text padding="5px"  alignItems="center" borderRadius="10px 0px 0px 10px" borderRight="1px solid" backgroundColor="#01bf71" fontWeight={700}>Email: </Text>
+                <Text margin="5px">karlwdorogy@gmail.com</Text> 
+              </Flex>
+              <Flex 
+                marginTop="1rem" 
+                border="1px solid"
+                borderRadius="10px" 
+                width="100%"
+                > 
+                <Text padding="5px"  alignItems="center" borderRadius="10px 0px 0px 10px" borderRight="1px solid" backgroundColor="#01bf71" fontWeight={700}>Verified: </Text>
+                <Text margin="5px">True</Text> 
+              </Flex>
+              <Divider marginBottom="1rem" size="medium" display="flex" orientation="horizontal"/>
+            </Flex>
+          </Card>
+
+          <Divider size="medium" display="flex" orientation="horizontal"/>
+          
+          <Card variation='elevated' borderRadius="1rem" border="1px solid" paddingBottom="1px" paddingTop="1px" paddingLeft="10%" paddingRight="10%" >
+            <Flex 
+              direction="row"
+              alignItems="center"
+              paddingLeft="7px"
+              paddingRight="7px"
+              >
+              <h1>Group - </h1>
+              <NavBtn>
+                <ButtonLinks to="/">Create New Group</ButtonLinks>
+              </NavBtn>
+            </Flex>
+            <Divider size="medium" display="flex" orientation="horizontal"/>
+            <Flex direction="column"
+              alignItems="flex-start"
+              >
+              <Flex 
+                marginTop="1rem" 
+                border="1px solid"
+                borderRadius="10px" 
+                width="100%"
+                > 
+                <Text padding="5px"  alignItems="center" borderRadius="10px 0px 0px 10px" borderRight="1px solid" backgroundColor="#01bf71" fontWeight={700}>Name: </Text>
+                <Text margin="5px">GHG Developers</Text> 
+              </Flex>
+              <Flex 
+                marginTop="1rem" 
+                border="1px solid"
+                borderRadius="10px" 
+                width="100%"
+                > 
+                <Text padding="5px"  alignItems="center" borderRadius="10px 0px 0px 10px" borderRight="1px solid" backgroundColor="#01bf71" fontWeight={700}>Role: </Text>
+                <Text margin="5px">Group Member</Text> 
+              </Flex>
+              <Divider marginBottom="1rem" size="medium" display="flex" orientation="horizontal"/>
+            </Flex>
+          </Card>
+
+        </Flex>
       </>
     );
 }
